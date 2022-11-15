03ba4d2cf384c94af738cb0817ea5a6cfcaa8fd6
Nate <nate.d.gage@gmail.com>
2022-11-15 11:52:03 -0700

    removed import on healthSelectors.js

--- src/features/inventory/selectors/healthSelectors.js
+++ src/features/inventory/selectors/healthSelectors.js
@@ -1,8 +1,6 @@
 /**
  * Selectors for HealthCounter.js are moved to its own file, but not the Inventory Selectors.
- * This is just to demo that the selectors can be imported from a separate Selectors file, or just
- * from the slice file.
+ * This is just to demo that the selectors can be imported from a separate Selectors file.
  */
-import initialState from "../healthSlice";
 
 export const selectHealth = (state) => state.health.health;
